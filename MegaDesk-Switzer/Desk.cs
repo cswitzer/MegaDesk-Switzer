2739ce6cfd5b1f49edd545d77457351e8d257ddf
chaseswizzleswiz@gmail.com <chaseswizzleswiz@gmail.com>
2022-05-05 13:10:15 -0600

    added desk constants

--- MegaDesk-Switzer/Desk.cs
+++ MegaDesk-Switzer/Desk.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace MegaDesk_Switzer
 {
+    public enum DesktopMaterial
+    {
+        Laminate,
+        Oak,
+        Rosewood,
+        Veneer,
+        Pine
+    }
     internal class Desk
     {
+        public const short MIN_WIDTH = 24;
+        public const short MAX_WIDTH = 96;
+        public const short MIN_DEPTH = 12;
+        public const short MAX_DEPTH = 48;
+        public const short MIN_DESK_DRAWERS = 0;
+        public const short MAX_DESK_DRAWERS = 0;
+
+        public decimal Width { get; set; }
+        public decimal Depth { get; set; }
+        public int NumberOfDrawers { get; set; }
+        public DesktopMaterial DesktopMaterial { get; set; }
     }
 }
