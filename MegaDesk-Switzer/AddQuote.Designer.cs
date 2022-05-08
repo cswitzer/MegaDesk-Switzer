8bef94ae8d6d1ef47a4a9a856f97f11c8868e8bf
Chase <chasehillig@yahoo.com>
2022-05-07 22:36:29 -0600

    implemented DeskQuote class

--- MegaDesk-Switzer/AddQuote.Designer.cs
+++ MegaDesk-Switzer/AddQuote.Designer.cs
@@ -28,20 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.widthLabel = new System.Windows.Forms.Label();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.widthTextBox = new System.Windows.Forms.TextBox();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // AddQuoteMenu
+            // widthLabel
+            // 
+            this.widthLabel.AutoSize = true;
+            this.widthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.widthLabel.Location = new System.Drawing.Point(125, 75);
+            this.widthLabel.Name = "widthLabel";
+            this.widthLabel.Size = new System.Drawing.Size(63, 24);
+            this.widthLabel.TabIndex = 0;
+            this.widthLabel.Text = "Width:";
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heightLabel.Location = new System.Drawing.Point(500, 75);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(70, 24);
+            this.heightLabel.TabIndex = 1;
+            this.heightLabel.Text = "Height:";
+            // 
+            // widthTextBox
+            // 
+            this.widthTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.widthTextBox.Location = new System.Drawing.Point(129, 120);
+            this.widthTextBox.Name = "widthTextBox";
+            this.widthTextBox.Size = new System.Drawing.Size(100, 29);
+            this.widthTextBox.TabIndex = 2;
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heightTextBox.Location = new System.Drawing.Point(504, 120);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(100, 29);
+            this.heightTextBox.TabIndex = 3;
+            // 
+            // AddQuote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "AddQuoteMenu";
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.widthTextBox);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.widthLabel);
+            this.Name = "AddQuote";
             this.Text = "Add Quote";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AddQuoteMenu_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label widthLabel;
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.TextBox widthTextBox;
+        private System.Windows.Forms.TextBox heightTextBox;
     }
 }
--- MegaDesk-Switzer/DeskQuote.cs
+++ MegaDesk-Switzer/DeskQuote.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace MegaDesk_Switzer
 {
+    enum RushOrderType
+    {
+        Express,
+        Firstclass,
+        Priority,
+        Normal
+    }
     internal class DeskQuote
     {
+        public string CustomerName { get; set; }
+        public DateTime QuoteDate { get; set; }
+        public RushOrderType OrderType { get; set; }
+        public decimal QuotePrice { get; set; }
+        public Desk Desk { get; set; }
+
+        public decimal CalcQuote()
+        {
+            return 0;
+        }
     }
 }
